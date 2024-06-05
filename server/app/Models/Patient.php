415340c25e474a96b60b824b781252272a786932
Snaarze <panlasiguijeremyglenn49@gmail.com>
2024-06-05 14:13:29 +0800

    first commit for Patient

---
+++ server/app/Models/Patient.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model {
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'date_of_birth',
+        'gender',
+        'address',
+        'phone',
+        'email',
+        'emergency_contanct',
+        'medical_history'
+    ];
+}
