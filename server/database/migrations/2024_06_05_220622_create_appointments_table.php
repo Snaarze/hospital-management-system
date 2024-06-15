073b920d2576975677fc06edcfdba6aae1160c97
Oliver Quiroz <oliver.emmanuel.quiroz@gmail.com>
2024-06-15 21:50:55 +0800

    update migration for appointments

--- server/database/migrations/2024_06_05_220622_create_appointments_table.php
+++ server/database/migrations/2024_06_05_220622_create_appointments_table.php
@@ -4,28 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
-    {
+    public function up(): void {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('patient_id');
+            $table->foreignId('doctor_id');
+            $table->date('appointment_date');
+            $table->string('status');
+            $table->string('reason');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
-    {
+    public function down(): void {
         Schema::dropIfExists('appointments');
     }
 };
