94f41a7b17b4efa836aca6161ef81761a94922d2
Oliver Quiroz <oliver.emmanuel.quiroz@gmail.com>
2024-06-15 21:53:11 +0800

    update appointment.php model

--- server/app/Models/Appointment.php
+++ server/app/Models/Appointment.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Appointment extends Model
-{
+class Appointment extends Model {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+        'appointment_date',
+        'status',
+        'reason'
+    ];
 }
